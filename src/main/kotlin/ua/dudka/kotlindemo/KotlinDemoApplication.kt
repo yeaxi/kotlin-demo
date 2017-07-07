77a890b962f6b20e1c2defd88340007fb5a88465
rostislav.dudka <rostislav.dudka@gmail.com>
2017-07-07 13:58:01 +0300

    -initial commit

---
+++ src/main/kotlin/ua/dudka/kotlindemo/KotlinDemoApplication.kt
@@ -0,0 +1,11 @@
+package ua.dudka.kotlindemo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class KotlinDemoApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(KotlinDemoApplication::class.java, *args)
+}
---
+++ src/test/kotlin/ua/dudka/kotlindemo/KotlinDemoApplicationTests.kt
@@ -0,0 +1,16 @@
+package ua.dudka.kotlindemo
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KotlinDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
